be5c2d41ee3cda094a55a7ee838a4088084f3d4e
Kymemia <kimemiachristopherr@gmail.com>
2024-08-01 16:58:56 +0300

    basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function that returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    method definition that returns
+    a concatenated string
+    """
+    return str1 + str2
