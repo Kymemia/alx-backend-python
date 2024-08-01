ac325c47747cb96daa3d71a263d03e5251576453
Kymemia <kimemiachristopherr@gmail.com>
2024-08-01 16:44:12 +0300

    basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Function that adds 2 float numbers and returns their sum
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Method definition to return the sum
+    of two float arguments
+    """
+    return a + b
