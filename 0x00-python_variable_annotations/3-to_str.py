79fcc4c97641634d0869a61fc2ac508a1c49a487
Kymemia <kimemiachristopherr@gmail.com>
2024-08-01 17:35:32 +0300

    basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+function that takes a float <n> as argument
+and returns the string representation
+of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    method definition that takes a float
+    as argument and returns string representation
+    of the float
+    """
+    return str(n)
