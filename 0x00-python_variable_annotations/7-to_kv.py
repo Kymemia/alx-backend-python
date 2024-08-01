e9b85398625e3c8c4fdad7a9b7977b0c6afdcf51
Kymemia <kimemiachristopherr@gmail.com>
2024-08-01 18:45:08 +0300

    complex types - string and int/float to tuple run I => documentation fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -11,6 +11,11 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    takes string (k) and int/float (v) as args. Returns tuple
+    First element => string, k
+    Second element => square of int/float (v)
+    """
     squared = v ** 2
     if squared % 1 == 0:
         return (k, int(squared))
