2fb9644971c1931d7cbb8577b19a25513a35f734
Kymemia <kimemiachristopherr@gmail.com>
2024-08-01 17:21:28 +0300

    basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+function that takes a float as argument
+and returns the floor of the float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    method definition that returns
+    the floor of the float
+    """
+    return math.floor(n)
