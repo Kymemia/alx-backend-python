61580e84225591168cccb23fe2713f9bec5a125a
Kymemia <kimemiachristopherr@gmail.com>
2024-08-09 18:32:56 +0300

    annotatation fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,9 +7,10 @@ then yields a andom number between 0 and 10
 """
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     method definition
     """
