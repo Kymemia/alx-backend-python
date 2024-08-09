6ba26354664f0be10344cfb593fb447628648bff
Kymemia <kimemiachristopherr@gmail.com>
2024-08-09 18:29:07 +0300

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+this is a coroutine named async_generator
+it loops 10 times, each time asynchornously waiting 1 second,
+then yields a andom number between 0 and 10
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    method definition
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
