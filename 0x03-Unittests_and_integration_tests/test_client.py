b771147f5d135e0a37e192b7902cdc94c9593cca
Kymemia <kimemiachristopherr@gmail.com>
2024-08-27 01:23:06 +0300

    more patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,7 @@ test_case for client.py file
 import unittest
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
-from typing import Dict
+from typing import Dict, List
 from client import GithubOrgClient
 
 
@@ -45,6 +45,23 @@ class TestGithubOrgClient(unittest.TestCase):
             result = client._public_repos_url
             expected_result = "https://api.github.com/orgs/google/repos"
             self.assertEqual(result, expected_result)
+
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock)
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        """
+        this is a test that returns a list of repositories
+        based on mocked payload
+        """
+        test_payload = [{"name": "first_repo"}, {"name": "second_repo"}]
+        mock_get_json.return_value = test_payload
+
+        mock_public_repos_url.return_value = "https://api.github.com/orgs/test/repos"
+        client = GithubOrgClient("test")
+        repos = client.public_repos()
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once_with("https://api.github.com/orgs/test/repos")
+        self.assertEqual(repos, ["first_repo", "second_repo"])
 
 
 if __name__ == "__main__":
